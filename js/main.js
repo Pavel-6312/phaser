35bdfd06b50ed0413d510b8e340a3603c39fcbcd
P <morr.conceptart@gmail.com>
2020-04-30 21:56:36 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var config = {
         type: Phaser.AUTO,
-        width: 600,
+        width: 800,
         height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
         scene: {
             preload: preload,
             create: create,
@@ -9,33 +16,99 @@ var config = {
         }
     };
 
+// Variables
     var game = new Phaser.Game(config);
+    var platforms;
+    var player;
 
+//Preload
     function preload ()
     {
         this.load.image('sky', 'assets/sky.png');
+        this.load.image('bg1', 'assets/bg1.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
+            
         );
+        this.load.spritesheet('rat', 'assets/rat.png',
+            { frameWidth: 100, frameHeight: 100 }
+            
+        );
+
+        this.load.image('grave-bg', 'assets/graveyard/png/BG.png');
     }
 
+//Create
     function create ()
     {
-        this.add.image(300, 300, 'sky');
-        this.add.image(110, 110, 'dude').setScale(2);
 
+//Sprites        
+        this.add.image(400, 300, 'grave-bg').setScale(0.5);
+
+
+//Platforms
         platforms = this.physics.add.staticGroup();
+        platforms.create(162, 584, 'ground');
+        platforms.create(384, 584, 'ground');
+        platforms.create(546, 584, 'ground');
+        platforms.create(708, 584, 'ground');
 
-        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        platforms.create(500, 450, 'ground');
+        platforms.create(50, 300, 'ground');
+        platforms.create(650, 200, 'ground');
 
-        platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        platforms.create(750, 220, 'ground');
+
+//Player
+        player = this.physics.add.sprite(100, 450, 'dude');
+        player.setCollideWorldBounds(true);
+        player.body.setGravityY(500);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+//Collider
+        this.physics.add.collider(player, platforms);
+
+//Keyboard control
+        cursors = this.input.keyboard.createCursorKeys();
     }
 
+//Update
     function update ()
     {
+        if (cursors.left.isDown){
+            player.setVelocityX(-240);
+            player.anims.play('left', true);
+        } 
+        else if (cursors.right.isDown){
+            player.setVelocityX(240);
+            player.anims.play('right', true);
+        } 
+        else{
+            player.setVelocityX(0);
+            player.anims.play('turn');
+        }
+        if (cursors.up.isDown && player.body.touching.down){
+            player.setVelocityY(-550);
+        }
     }
